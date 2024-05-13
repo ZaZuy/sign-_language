aa7217ab4f6b935f9b31edd3c510dcdebf125403
ZaZuy <137234122+ZaZuy@users.noreply.github.com>
2024-05-13 17:22:49 +0700

    Update holistic-cam.py

--- 2Caps/holistic-cam.py
+++ 2Caps/holistic-cam.py
@@ -1,359 +1,92 @@
-# import cv2
-# import mediapipe as mp
-# import matplotlib.pyplot as plt
-# import pandas as pd
-# import numpy as np
-# mp_drawing = mp.solutions.drawing_utils
-# mp_drawing_styles = mp.solutions.drawing_styles
-# mp_holistic = mp.solutions.holistic
+# import  pandas as pd
+# import  numpy as np
+# import tflite_runtime.interpreter as tflite
+# ROWS_PER_FRAME = 543  # number of landmarks per frame
 #
-# def create_frame_landmark_df(results,frame,xyz):
-#   xyz_skel = xyz[['type','landmark_index']].drop_duplicates().reset_index(drop=True) \
-#             .copy()
-#   face = pd.DataFrame()
-#   pose = pd.DataFrame()
-#   left_hand = pd.DataFrame()
-#   right_hand = pd.DataFrame()
+# def load_relevant_data_subset(pq_path):
+#     data_columns = ['x', 'y', 'z']
+#     data = pd.read_parquet(pq_path, columns=data_columns)
+#     n_frames = int(len(data) / ROWS_PER_FRAME)
+#     data = data.values.reshape(n_frames, ROWS_PER_FRAME, len(data_columns))
+#     return data.astype(np.float32)
 #
-#   if results.face_landmarks:
-#     for i,point in enumerate(results.face_landmarks.landmark):
-#       face.loc[i,['x','y','z']] = [point.x, point.y,point.z]
-#   if results.pose_landmarks:
-#     for i,point in enumerate(results.pose_landmarks.landmark):
-#       pose.loc[i,['x','y','z']] = [point.x, point.y,point.z]
-#   if results.left_hand_landmarks:
-#     for i,point in enumerate(results.left_hand_landmarks.landmark):
-#       left_hand.loc[i,['x','y','z']] = [point.x, point.y,point.z]
-#   if results.right_hand_landmarks:
-#     for i,point in enumerate(results.right_hand_landmarks.landmark):
-#       right_hand.loc[i,['x','y','z']] = [point.x, point.y, point.z]
-#   face = face.reset_index()\
-#       .rename(columns={'index':'landmark_index'})\
-#       .assign(type='face')
-#   pose = pose.reset_index() \
-#     .rename(columns={'index': 'landmark_index'}) \
-#     .assign(type='pose')
-#   left_hand = left_hand.reset_index() \
-#     .rename(columns={'index': 'landmark_index'}) \
-#     .assign(type='left_hand')
-#   right_hand = right_hand.reset_index() \
-#     .rename(columns={'index': 'landmark_index'}) \
-#     .assign(type='right_hand')
-#   landmarks = pd.concat([face,pose,left_hand,right_hand]).reset_index(drop=True)
-#   landmarks = xyz_skel.merge(landmarks,on=['type','landmark_index'], how='left')
-#   landmarks = landmarks.assign(frame=frame)
-#   return landmarks
-# def do_capture_loop(xyz):
-#   all_landmarks =[]
-#   # For webcam input:
-#   cap = cv2.VideoCapture(0)
-#   with mp_holistic.Holistic(
-#       min_detection_confidence=0.5,
-#       min_tracking_confidence=0.5) as holistic:
-#     frame = 0
-#     while cap.isOpened():
-#       frame +=1
-#       success, image = cap.read()
-#       if not success:
-#         print("Ignoring empty camera frame.")
-#         # If loading a video, use 'break' instead of 'continue'.
-#         continue
 #
-#       # To improve performance, optionally mark the image as not writeable to
-#       # pass by reference.
-#       image.flags.writeable = False
-#       image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#       results = holistic.process(image)
+# interpreter = tflite.Interpreter("model.tflite")
+# found_signatures = list(interpreter.get_signature_list().keys())
+# prediction_fn = interpreter.get_signature_runner("serving_default")
 #
-#       landmarks = create_frame_landmark_df(results,frame,xyz)
-#       all_landmarks.append(landmarks)
+# # output = prediction_fn(inputs=demo_raw_data)
+# # sign = output['outputs'].argmax()
 #
-#       image.flags.writeable = True
-#       image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-#       mp_drawing.draw_landmarks(
-#         image,
-#         results.face_landmarks,
-#         mp_holistic.FACEMESH_CONTOURS,
-#         landmark_drawing_spec=None,
-#         connection_drawing_spec=mp_drawing_styles
-#         .get_default_face_mesh_contours_style())
-#       mp_drawing.draw_landmarks(
-#         image,
-#         results.pose_landmarks,
-#         mp_holistic.POSE_CONNECTIONS,
-#         landmark_drawing_spec=mp_drawing_styles
-#         .get_default_pose_landmarks_style())
-#       # Flip the image horizontally for a selfie-view display.
-#       cv2.imshow('MediaPipe Holistic', cv2.flip(image, 1))
-#       if cv2.waitKey(5) & 0xFF == 27:
-#         break
-#   return all_landmarks
+# train = pd.read_csv('train.csv')
+# pq_file = 'output_0_27.parquet'
+# train['sign_ord'] = train['sign'].astype('category').cat.codes
+# # Dictionaries to translate sign <-> ordinal encoded sign
+# SIGN2ORD = train[['sign', 'sign_ord']].set_index('sign').squeeze().to_dict()
+# ORD2SIGN = train[['sign_ord', 'sign']].set_index('sign_ord').squeeze().to_dict()
+# xyz = load_relevant_data_subset(pq_file)
+# predict = prediction_fn(inputs=xyz)
+# print(predict['outputs'])
+# sign = predict['outputs'].argmax()
+# predicted_label = ORD2SIGN[sign]
+# predicted_value = predict['outputs'][sign]
+# accuracy = predicted_value * 100 / predict['outputs'].sum()
 #
-# if __name__ == "__main__":
-#   pq_file = '10042041.parquet'
-#   xyz = pd.read_parquet(pq_file)
-#   landmarks = do_capture_loop(xyz)
-#   landmarks = pd.concat(landmarks).reset_index(drop=True).to_parquet('output.parquet')
+# print("Predicted Action:", predicted_label)
+# print("Accuracy:", accuracy, "%")
+# # print(train['sign'].unique)
+#
+import glob
+import os
+parquet_files = glob.glob("*.parquet")
 
+num_parquet_files = len(parquet_files)
+from collections import Counter
 import pandas as pd
-import cv2
-import mediapipe as mp
+import numpy as np
+import tflite_runtime.interpreter as tflite
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_holistic = mp.solutions.holistic
+ROWS_PER_FRAME = 543  # số lượng landmarks mỗi frame
 
-def create_frame_landmark_df(results, frame, xyz):
-    xyz_skel = xyz[['type','landmark_index']].drop_duplicates().reset_index(drop=True).copy()
-    face = pd.DataFrame()
-    pose = pd.DataFrame()
-    left_hand = pd.DataFrame()
-    right_hand = pd.DataFrame()
+def load_relevant_data_subset(pq_path):
+    data_columns = ['x', 'y', 'z']
+    data = pd.read_parquet(pq_path, columns=data_columns)
+    n_frames = int(len(data) / ROWS_PER_FRAME)
+    data = data.values.reshape(n_frames, ROWS_PER_FRAME, len(data_columns))
+    return data.astype(np.float32)
 
-    if results.face_landmarks:
-        for i, point in enumerate(results.face_landmarks.landmark):
-            face.loc[i,['x','y','z']] = [point.x, point.y, point.z]
-    if results.pose_landmarks:
-        for i, point in enumerate(results.pose_landmarks.landmark):
-            pose.loc[i,['x','y','z']] = [point.x, point.y, point.z]
-    if results.left_hand_landmarks:
-        for i, point in enumerate(results.left_hand_landmarks.landmark):
-            left_hand.loc[i,['x','y','z']] = [point.x, point.y, point.z]
-    if results.right_hand_landmarks:
-        for i, point in enumerate(results.right_hand_landmarks.landmark):
-            right_hand.loc[i,['x','y','z']] = [point.x, point.y, point.z]
+interpreter = tflite.Interpreter("model.tflite")
+found_signatures = list(interpreter.get_signature_list().keys())
+prediction_fn = interpreter.get_signature_runner("serving_default")
 
-    face = face.reset_index().rename(columns={'index':'landmark_index'}).assign(type='face')
-    pose = pose.reset_index().rename(columns={'index': 'landmark_index'}).assign(type='pose')
-    left_hand = left_hand.reset_index().rename(columns={'index': 'landmark_index'}).assign(type='left_hand')
-    right_hand = right_hand.reset_index().rename(columns={'index': 'landmark_index'}).assign(type='right_hand')
+train = pd.read_csv('train.csv')
+train['sign_ord'] = train['sign'].astype('category').cat.codes
+SIGN2ORD = train[['sign', 'sign_ord']].set_index('sign').squeeze().to_dict()
+ORD2SIGN = train[['sign_ord', 'sign']].set_index('sign_ord').squeeze().to_dict()
 
-    landmarks = pd.concat([face, pose, left_hand, right_hand]).reset_index(drop=True)
-    landmarks = xyz_skel.merge(landmarks, on=['type', 'landmark_index'], how='left')
-    landmarks = landmarks.assign(frame=frame)
-    return landmarks
+n = num_parquet_files
+my_predict = []
+for i in range(n-1):
+    pq_file = f'output_{i}.parquet'
+    xyz = load_relevant_data_subset(pq_file)
+    predict = prediction_fn(inputs=xyz)
+    sign = predict['outputs'].argmax()
+    predicted_label = ORD2SIGN[sign]
+    predicted_value = predict['outputs'][sign]
+    accuracy = predicted_value * 100 / predict['outputs'].sum()
 
-def do_capture_loop(xyz, duration_per_file=2, overlap=1):
-    all_landmarks = []
-    cap = cv2.VideoCapture(0)
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    frame = 0
-    file_index = 0
+    my_predict.append(predicted_label)
+    print(f"Predicted Action for {pq_file}: {predicted_label}, Accuracy: {accuracy:.2f}%")
+    os.remove(pq_file)
+label_counts = Counter(my_predict)
 
-    overlap_frames = int(fps * overlap)
-    frames_per_file = int(total_frames / (total_frames / (duration_per_file * fps) + overlap_frames))
 
-    with mp_holistic.Holistic(
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as holistic:
+for label, count in label_counts.items():
+    if count < 2:
+        my_predict = [item for item in my_predict if item != label]
+unique_chars = list(set(my_predict))
+while "TV" in unique_chars:
+    unique_chars.remove("TV")
+print("Các nhãn sau khi loại bỏ:", unique_chars)
 
-        while True:
-            frame += 1
-            success, image = cap.read()
-            if not success:
-                print("Failed to read frame.")
-                break
-
-            image.flags.writeable = False
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            results = holistic.process(image)
-
-            landmarks = create_frame_landmark_df(results, frame, xyz)
-            all_landmarks.append(landmarks)
-
-            if frame % frames_per_file == 0 or frame == 1:
-                output_file = f'output_{file_index}.parquet'
-                combined_landmarks = pd.concat(all_landmarks).reset_index(drop=True)
-                combined_landmarks.to_parquet(output_file)
-                all_landmarks = []
-                file_index += 1
-            elif frame % frames_per_file >= frames_per_file - overlap_frames:
-                all_landmarks = all_landmarks[1:]
-
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            mp_drawing.draw_landmarks(
-                image,
-                results.face_landmarks,
-                mp_holistic.FACEMESH_CONTOURS,
-                landmark_drawing_spec=None,
-                connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style())
-            mp_drawing.draw_landmarks(
-                image,
-                results.pose_landmarks,
-                mp_holistic.POSE_CONNECTIONS,
-                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-            cv2.imshow('MediaPipe Holistic', cv2.flip(image, 1))
-            if cv2.waitKey(5) & 0xFF == 27:
-                break
-
-    cap.release()
-    cv2.destroyAllWindows()
-    if all_landmarks:  # Save any remaining landmarks after the loop ends
-        output_file = f'output_{file_index}.parquet'
-        combined_landmarks = pd.concat(all_landmarks).reset_index(drop=True)
-        combined_landmarks.to_parquet(output_file)
-    return all_landmarks
-
-if __name__ == "__main__":
-    pq_file = '10042041.parquet'
-    xyz = pd.read_parquet(pq_file)
-    duration_per_file = 2
-    overlap = 1
-    landmarks = do_capture_loop(xyz, duration_per_file, overlap)
-
-#test
-# import base64
-# import dataset
-# import tempfile
-# import os
-#
-# def do_capture_loop(xyz, video_base64, duration_per_file=2, overlap=1):
-#     all_landmarks = []
-#     video_bytes = base64.b64decode(video_base64)
-#
-#     # Tạo một tệp tạm thời để lưu trữ video
-#     temp_file_handle, temp_file_path = tempfile.mkstemp()
-#     with open(temp_file_path, "wb") as temp_file:
-#         temp_file.write(video_bytes)
-#
-#     cap = cv2.VideoCapture(temp_file_path)
-#     with mp_holistic.Holistic(
-#             min_detection_confidence=0.5,
-#             min_tracking_confidence=0.5) as holistic:
-#         frame = 0
-#         file_index = 0
-#         frame_per_file = duration_per_file * cap.get(cv2.CAP_PROP_FPS)
-#         overlap_frames = overlap * cap.get(cv2.CAP_PROP_FPS)
-#         while cap.isOpened():
-#             frame += 1
-#             success, image = cap.read()
-#             if not success:
-#                 print("End of video.")
-#                 break
-#
-#             image.flags.writeable = False
-#             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#             results = holistic.process(image)
-#
-#             landmarks = create_frame_landmark_df(results, frame, xyz)
-#             all_landmarks.append(landmarks)
-#
-#             if frame % frame_per_file == 0:
-#                 output_file = f'output_{file_index}.parquet'
-#                 combined_landmarks = pd.concat(all_landmarks).reset_index(drop=True)
-#                 combined_landmarks.to_parquet(output_file)
-#                 all_landmarks = []
-#                 file_index += 1
-#             elif frame % frame_per_file >= frame_per_file - overlap_frames:
-#                 all_landmarks = all_landmarks[1:]
-#
-#             if cv2.waitKey(1) & 0xFF == 27:
-#                 break
-#     cap.release()
-#
-#     # Xóa tệp tạm thời
-#     os.close(temp_file_handle)
-#     os.unlink(temp_file_path)
-#
-#     return all_landmarks
-#
-#
-# if __name__ == "__main__":
-#     pq_file = '10042041.parquet'
-#     xyz = pd.read_parquet(pq_file)
-#     video_base64 = dataset.video_base64  # Chuỗi base64 đại diện cho video
-#     duration_per_file = 2
-#     overlap = 1
-#     landmarks = do_capture_loop(xyz, video_base64, duration_per_file, overlap)
-#     if landmarks:
-#         file_index = 0
-#         for i, landmarks_batch in enumerate(landmarks):
-#             output_file = f'output_{i}.parquet'
-#             landmarks_batch.to_parquet(output_file)
-#         file_index += 1
-
-# import cv2
-# import mediapipe as mp
-# import pandas as pd
-# import numpy as np
-# import time
-#
-# mp_drawing = mp.solutions.drawing_utils
-# mp_drawing_styles = mp.solutions.drawing_styles
-# mp_holistic = mp.solutions.holistic
-#
-# def create_frame_landmark_df(results, frame, xyz):
-#     xyz_skel = xyz[['type', 'landmark_index']].drop_duplicates().reset_index(drop=True).copy()
-#     face = pd.DataFrame()
-#     pose = pd.DataFrame()
-#     left_hand = pd.DataFrame()
-#     right_hand = pd.DataFrame()
-#
-#     if results.face_landmarks:
-#         for i, point in enumerate(results.face_landmarks.landmark):
-#             face.loc[i, ['x', 'y', 'z']] = [point.x, point.y, point.z]
-#     if results.pose_landmarks:
-#         for i, point in enumerate(results.pose_landmarks.landmark):
-#             pose.loc[i, ['x', 'y', 'z']] = [point.x, point.y, point.z]
-#     if results.left_hand_landmarks:
-#         for i, point in enumerate(results.left_hand_landmarks.landmark):
-#             left_hand.loc[i, ['x', 'y', 'z']] = [point.x, point.y, point.z]
-#     if results.right_hand_landmarks:
-#         for i, point in enumerate(results.right_hand_landmarks.landmark):
-#             right_hand.loc[i, ['x', 'y', 'z']] = [point.x, point.y, point.z]
-#     face = face.reset_index().rename(columns={'index': 'landmark_index'}).assign(type='face')
-#     pose = pose.reset_index().rename(columns={'index': 'landmark_index'}).assign(type='pose')
-#     left_hand = left_hand.reset_index().rename(columns={'index': 'landmark_index'}).assign(type='left_hand')
-#     right_hand = right_hand.reset_index().rename(columns={'index': 'landmark_index'}).assign(type='right_hand')
-#     landmarks = pd.concat([face, pose, left_hand, right_hand]).reset_index(drop=True)
-#     landmarks = xyz_skel.merge(landmarks, on=['type', 'landmark_index'], how='left')
-#     landmarks = landmarks.assign(frame=frame)
-#     return landmarks
-#
-# def do_capture_loop(xyz):
-#     all_landmarks = []
-#     cap = cv2.VideoCapture(0)
-#     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-#         frame = 0
-#         last_capture_time = time.time()
-#         while cap.isOpened():
-#             frame += 1
-#             success, image = cap.read()
-#             if not success:
-#                 print("Ignoring empty camera frame.")
-#                 continue
-#
-#             image.flags.writeable = False
-#             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#             results = holistic.process(image)
-#
-#             landmarks = create_frame_landmark_df(results, frame, xyz)
-#             all_landmarks.append(landmarks)
-#
-#             image.flags.writeable = True
-#             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-#             mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
-#                                       landmark_drawing_spec=None,
-#                                       connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style())
-#             mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-#                                       landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-#
-#             cv2.imshow('MediaPipe Holistic', cv2.flip(image, 1))
-#             if cv2.waitKey(5) & 0xFF == 27:
-#                 break
-#
-#             # Check if 2 seconds have passed since last capture
-#             if time.time() - last_capture_time >= 1:
-#                 last_capture_time = time.time()
-#                 pd.concat(all_landmarks).reset_index(drop=True).to_parquet(f'output_{frame}.parquet')
-#                 #pd.concat(all_landmarks).reset_index(drop=True).to_parquet(f'output_{int(frame / 30)}.parquet')
-#
-#     return all_landmarks
-#
-# if __name__ == "__main__":
-#     pq_file = '10042041.parquet'
-#     xyz = pd.read_parquet(pq_file)
-#     landmarks = do_capture_loop(xyz)
 
